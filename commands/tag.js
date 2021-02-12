b6f5e1fe4c5e776806ce94733fb842f5a4d82402
paz <paz@paz.yt>
2021-02-12 23:51:58 +0800

    databases, (almost) tags, fixed eslint

---
+++ commands/tag.js
@@ -0,0 +1,78 @@
+const Discord = require("discord.js");
+const shared = require("../shared");
+
+module.exports = {
+  alias: ["tag", "t"],
+  description: "View, edit, list, delete or create tags.",
+  usage: "<tag name / list / delete [tag] / edit [tag]> [content ...]",
+  args: true,
+  async execute (
+    message,
+    args,
+    latency,
+    commands,
+    client,
+    prefixes,
+    globalPrefix,
+    Tags
+  ) {
+    const embed = new Discord.MessageEmbed();
+    const tagName = args.shift();
+    const tagDescription = args.join(" ");
+    if (tagName === "list") {
+      const tagList = await Tags.findAll({
+        attributes: ["name"],
+        where: { owner: message.author.id }
+      });
+      const tagString = tagList.map((t) => t.name).join(", ") || "No tags set.";
+      return message.channel.send(`List of tags: ${tagString}`);
+    } else if (tagName === "delete") {
+      const rowCount = await Tags.destroy({
+        where: { name: tagName, owner: message.author.id }
+      });
+      if (!rowCount) { return message.reply("That tag did not exist, or you do not own it."); }
+      return message.reply("Tag deleted.");
+    } else if (tagName && args.length < 1) {
+      const tag = await Tags.findOne({ where: { name: tagName } });
+      if (tag) {
+        tag.increment("uses");
+        return message.channel.send(tag.get("description"));
+      }
+      return message.reply(`Could not find tag: ${tagName}`);
+    } else if (tagName === "edit") {
+      const affectedRows = await Tags.update(
+        {
+          description: tagDescription
+        },
+        {
+          where: { name: tagName, owner: message.author.id }
+        }
+      );
+      if (affectedRows > 0) {
+        return message.reply(`Tag ${tagName} was edited.`);
+      }
+      return message.reply(
+        `Could not find a tag with name ${tagName}, or you do not own it.`
+      );
+    } else {
+      try {
+        const tag = await Tags.create({
+          name: tagName,
+          description: tagDescription,
+          owner: message.author.id
+        });
+        return message.reply(`Tag ${tag.name} added.`);
+      } catch (e) {
+        if (e.name === "SequelizeUniqueConstraintError") {
+          return message.reply("That tag already exists.");
+        }
+        return message.reply("Something went wrong with adding a tag.");
+      }
+    }
+    /* embed.setFooter(
+      shared.createFooter(message, latency),
+      shared.createAvatar(message.author, "user")
+    );
+    message.channel.send(embed); */
+  }
+};
--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const fs = require("fs");
 // discord
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const Keyv = require('keyv');
+const Keyv = require("keyv");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(process.env.db_name, process.env.db_user, process.env.db_pass, {
+  host: process.env.db_host,
+  dialect: "mariadb",
+  logging: false
+});
+
 const globalPrefix = ",";
-
-const prefixes = new Keyv(process.env.keyv, { namespace: 'prefixes' });
-
-const cooldowns = new Discord.Collection();
+const prefixes = new Keyv(process.env.keyv, { namespace: "prefixes" });
 
 // commands
 client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
@@ -26,14 +31,32 @@ const shared = require("./shared");
 process.on("uncaughtException", (err) => shared.handleError(err));
 process.on("unhandledRejection", (err) => shared.handleError(err));
 
+// tables
+
+const Tags = sequelize.define("tags", {
+  name: {
+    type: Sequelize.STRING,
+    unique: true
+  },
+  description: Sequelize.TEXT,
+  owner: Sequelize.STRING,
+  uses: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    allowNull: false
+  }
+});
+
 // events
 
 client.on("ready", async () => {
+  Tags.sync();
+
   console.log(
     `Logged in as ${client.user.tag}!
     Serving ${client.guilds.cache.size} guilds & ${client.users.cache.size} users`
   );
-  let readyEmbed = new Discord.MessageEmbed();
+  const readyEmbed = new Discord.MessageEmbed();
   readyEmbed.setTitle(process.env.instance + " is now online");
   readyEmbed.setDescription(`Logged in as <@${client.user.id}>
   Serving ${client.guilds.cache.size} guilds & ${client.users.cache.size} users`);
@@ -41,26 +64,25 @@ client.on("ready", async () => {
 });
 
 client.on("message", async (message) => {
-  let latency = Date.now();
+  const latency = Date.now();
   let prefix;
-	if (message.guild) {
-		if (message.content.startsWith(globalPrefix)) {
-			prefix = globalPrefix;
-		} else {
-			const guildPrefix = await prefixes.get(message.guild.id);
-			if (message.content.startsWith(guildPrefix)) prefix = guildPrefix;
-		}
-		if (!prefix) return;
-	} else {
-		prefix = globalPrefix;
-	}
+  if (message.guild) {
+    if (message.content.startsWith(globalPrefix)) {
+      prefix = globalPrefix;
+    } else {
+      const guildPrefix = await prefixes.get(message.guild.id);
+      if (message.content.startsWith(guildPrefix)) prefix = guildPrefix;
+    }
+    if (!prefix) return;
+  } else {
+    prefix = globalPrefix;
+  }
 
   if (
     !message.content.startsWith(prefix) ||
     message.author.bot ||
     message.author.id === client.user.id
-  )
-    return;
+  ) { return; }
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
@@ -69,8 +91,8 @@ client.on("message", async (message) => {
 
   if (!cmd) return;
 
-  if(!cooldowns.has(cmd.alias[0])){
-    cooldowns.set(cmd.alias[0], new Discord.Collection())
+  if (!cooldowns.has(cmd.alias[0])) {
+    cooldowns.set(cmd.alias[0], new Discord.Collection());
   }
 
   const timestamps = cooldowns.get(cmd.alias[0]);
@@ -78,7 +100,7 @@ client.on("message", async (message) => {
 
   if (timestamps.has(message.author.id)) {
     const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-  
+
     if (latency < expirationTime) {
       const timeLeft = (expirationTime - latency) / 1000;
       return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command}\` command.`);
@@ -90,10 +112,13 @@ client.on("message", async (message) => {
 
   try {
     if (shared.hasPermission(message.guild, message.author, cmd.permission)) {
-      if(cmd.guild === true && !message.guild){
-        return message.channel.send("This command can only be used in a guild.")
+      if (cmd.guild === true && !message.guild) {
+        return message.channel.send("This command can only be used in a guild.");
       }
-      cmd.execute(message, args, latency, client.commands, client, prefixes, globalPrefix);
+      if (cmd.args && !args.length) {
+        return message.channel.send("Syntax: ``" + prefix + command + " " + cmd.usage + "``");
+      }
+      cmd.execute(message, args, latency, client.commands, client, prefixes, globalPrefix, Tags);
     }
   } catch (error) {
     console.error(error);
@@ -101,50 +126,50 @@ client.on("message", async (message) => {
   }
 });
 
-client.on('guildBanAdd', async (guild, user) => {
-	const fetchedLogs = await guild.fetchAuditLogs({
-		limit: 1,
-		type: 'MEMBER_BAN_ADD',
-	});
-	const banLog = fetchedLogs.entries.first();
-	if (!banLog) return console.log(`${user.tag} was banned from ${guild.name} but no audit log could be found.`);
-	const { executor, target } = banLog;
-	if (target.id === user.id) {
-		console.log(`${user.tag} got hit with the swift hammer of justice in the guild ${guild.name}, wielded by the mighty ${executor.tag}`);
-	} else {
-		console.log(`${user.tag} got hit with the swift hammer of justice in the guild ${guild.name}, audit log fetch was inconclusive.`);
-	}
+client.on("guildBanAdd", async (guild, user) => {
+  const fetchedLogs = await guild.fetchAuditLogs({
+    limit: 1,
+    type: "MEMBER_BAN_ADD"
+  });
+  const banLog = fetchedLogs.entries.first();
+  if (!banLog) return console.log(`${user.tag} was banned from ${guild.name} but no audit log could be found.`);
+  const { executor, target } = banLog;
+  if (target.id === user.id) {
+    console.log(`${user.tag} got hit with the swift hammer of justice in the guild ${guild.name}, wielded by the mighty ${executor.tag}`);
+  } else {
+    console.log(`${user.tag} got hit with the swift hammer of justice in the guild ${guild.name}, audit log fetch was inconclusive.`);
+  }
 });
 
-client.on('guildMemberRemove', async member => {
-	const fetchedLogs = await member.guild.fetchAuditLogs({
-		limit: 1,
-		type: 'MEMBER_KICK',
-	});
-	const kickLog = fetchedLogs.entries.first();
-	if (!kickLog) return console.log(`${member.user.tag} left the guild, most likely of their own will.`);
-	const { executor, target } = kickLog;
-	if (target.id === member.id) {
-		console.log(`${member.user.tag} left the guild; kicked by ${executor.tag}?`);
-	} else {
-		console.log(`${member.user.tag} left the guild, audit log fetch was inconclusive.`);
-	}
+client.on("guildMemberRemove", async member => {
+  const fetchedLogs = await member.guild.fetchAuditLogs({
+    limit: 1,
+    type: "MEMBER_KICK"
+  });
+  const kickLog = fetchedLogs.entries.first();
+  if (!kickLog) return console.log(`${member.user.tag} left the guild, most likely of their own will.`);
+  const { executor, target } = kickLog;
+  if (target.id === member.id) {
+    console.log(`${member.user.tag} left the guild; kicked by ${executor.tag}?`);
+  } else {
+    console.log(`${member.user.tag} left the guild, audit log fetch was inconclusive.`);
+  }
 });
 
-client.on('messageDelete', async message => {
-	if (!message.guild) return;
-	const fetchedLogs = await message.guild.fetchAuditLogs({
-		limit: 1,
-		type: 'MESSAGE_DELETE',
-	});
-	const deletionLog = fetchedLogs.entries.first();
-	if (!deletionLog) return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
-	const { executor, target } = deletionLog;
-	if (target.id === message.author.id) {
-		console.log(`A message by ${message.author.tag} was deleted by ${executor.tag}.`);
-	}	else {
-		console.log(`A message by ${message.author.tag} was deleted, but we don't know by who.`);
-	}
+client.on("messageDelete", async message => {
+  if (!message.guild) return;
+  const fetchedLogs = await message.guild.fetchAuditLogs({
+    limit: 1,
+    type: "MESSAGE_DELETE"
+  });
+  const deletionLog = fetchedLogs.entries.first();
+  if (!deletionLog) return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
+  const { executor, target } = deletionLog;
+  if (target.id === message.author.id) {
+    console.log(`A message by ${message.author.tag} was deleted by ${executor.tag}.`);
+  } else {
+    console.log(`A message by ${message.author.tag} was deleted, but we don't know by who.`);
+  }
 });
 
 // login
