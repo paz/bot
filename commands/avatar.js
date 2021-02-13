0e90d03a93dc4a84db8e1082db3721f1c1f0fe67
paz <paz@paz.yt>
2021-02-13 12:29:39 +0800

    eslint

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,20 +5,20 @@ module.exports = {
   alias: ["avatar", "avi", "pfp", "icon"],
   description: "See your avatar",
   usage: "[id / @mention]",
-  async execute(message, args, latency, commands, client) {
-    let embed = new Discord.MessageEmbed();
+  async execute (message, args, latency, commands, client) {
+    const embed = new Discord.MessageEmbed();
     let user;
-    if(args.length > 0){
-        user = await shared.getUserArg(message, args, client);
-    }else{
-        user = message.author;
+    if (args.length > 0) {
+      user = await shared.getUserArg(message, args, client);
+    } else {
+      user = message.author;
     }
-    if(!user){ return message.channel.send("invalid user")};
-    let avatar = shared.createAvatar(user, "user");
-    embed.setTitle("``"+user.username+"``'s avatar");
+    if (!user) { return message.channel.send("invalid user"); };
+    const avatar = shared.createAvatar(user, "user");
+    embed.setTitle("``" + user.username + "``'s avatar");
     embed.setImage(avatar);
-    embed.setDescription("[Full Resolution]("+avatar+"?size=2048)");
+    embed.setDescription("[Full Resolution](" + avatar + "?size=2048)");
     embed.setFooter(shared.createFooter(message, latency), shared.createAvatar(message.author, "user"));
-    message.channel.send(embed)
-  },
+    message.channel.send(embed);
+  }
 };
--- commands/translate.js
+++ commands/translate.js
@@ -6,16 +6,16 @@ module.exports = {
   description: "Translate",
   usage: "<target> [source] <text to translate>",
   permission: "botowner",
-  execute(message, args, latency, commands, client) {
-    let embed = new Discord.MessageEmbed();
-    let source = args.shift();
-    let target = args.shift();
-    embed.setAuthor("Google Translate", "https://cdn.wccftech.com/wp-content/uploads/2018/01/Google-Logo.png")
-    shared.googleTranslate(args.join(' '), target, source).then(tr => {
-        embed.setDescription("```"+tr.text+"```")
-        embed.setTitle(tr.source+" -> "+tr.target);
-        embed.setFooter(shared.createFooter(message, latency), shared.createAvatar(message.author, "user"));
-        message.channel.send(embed)
-    })
-  },
+  execute (message, args, latency, commands, client) {
+    const embed = new Discord.MessageEmbed();
+    const source = args.shift();
+    const target = args.shift();
+    embed.setAuthor("Google Translate", "https://cdn.wccftech.com/wp-content/uploads/2018/01/Google-Logo.png");
+    shared.googleTranslate(args.join(" "), target, source).then(tr => {
+      embed.setDescription("```" + tr.text + "```");
+      embed.setTitle(tr.source + " -> " + tr.target);
+      embed.setFooter(shared.createFooter(message, latency), shared.createAvatar(message.author, "user"));
+      message.channel.send(embed);
+    });
+  }
 };
