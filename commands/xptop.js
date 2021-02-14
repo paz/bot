0913b3827c186ff4e9fe55875d861dcc2ea16601
paz <paz@paz.yt>
2021-02-14 20:37:03 +0800

    Add xp top

--- commands/xptop.js
+++ commands/xptop.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const { calculateLevel } = require("../shared");
 const shared = require("../shared");
 
 module.exports = {
@@ -19,6 +20,20 @@ module.exports = {
     Member
   ) {
     const embed = new Discord.MessageEmbed();
+    const topMembers = await Members.findAll({
+      limit: 10,
+      order: [["xp", "DESC"]],
+      where: { guild_id: message.guild.id }
+    });
+    let i = 1;
+    const fields = [];
+    topMembers.forEach(topMember => {
+      topMember = topMember.dataValues;
+      fields.push(i + "    <@" + topMember.user_id + ">     " + topMember.xp + "xp    Lvl. " + calculateLevel(topMember.xp));
+      i++;
+    });
+    embed.setTitle("Top Members of "+message.guild.name);
+    embed.setDescription(fields.join("\n"));
     embed.setFooter(
       shared.createFooter(message, latency),
       shared.createAvatar(message.author, "user")
