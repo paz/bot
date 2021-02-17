256c2f7679744b11370846673ba4b098e46f219a
paz <paz@paz.yt>
2021-02-17 12:58:04 +0800

    sick of cors

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "slackers.pw");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
